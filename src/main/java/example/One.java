7d87d0dba19e656cbcaecf722aa965bcbd32c22d
OwenTse <xiejw@yahoo.com>
2018-06-06 16:16:54 +0800

    初始化Demo

---
+++ src/main/java/example/One.java
@@ -0,0 +1,14 @@
+package example;
+
+public class One {
+    String message = "foo";
+
+    public String foo() {
+        return message;
+    }
+
+    public void uncoveredMethod() {
+        System.out.println(foo());
+    }
+
+}
